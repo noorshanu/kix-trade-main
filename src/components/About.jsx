b556c371ce0c84d3bafb52dc597fd3c8919239de
noor alam <51040978+noorshanu@users.noreply.github.com>
2023-12-05 20:03:01 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <section className=" relative overflow-y-hidden">
       <div className="container-wrapper relative z-50">
-        <div className="bg-[#a149a9] rounded-lg py-12">
+        <div className="bg-[#a149a9] rounded-lg py-12 px-2">
           <h1
             className=" archo-black txt-s  text-white text-2xl 
       font-semibold text-center"
@@ -12,14 +12,14 @@ function About() {
             ABOUT KIX
           </h1>
           <div className=" max-w-xl mx-auto text-center">
-            <p className="font-work font-medium text-white text-lg  py-5">
+            <p className="font-work font-medium text-white text-base sm:text-lg  py-5">
               Welcome to <span className="txt-s font-semibold">KiX </span> , a
               Live Football Gaming Platform where users can buy and sell digital
               assets representing the world’s most exciting footballers and
               compete to win based on their real-world match performances.
             </p>
 
-            <p className="font-work font-medium text-white text-lg">
+            <p className="font-work font-medium text-white text-base sm:text-lg">
               <span className="txt-s font-semibold">KiX </span> is Alpha Testing
               on the Polygon Testnet NOW and  <span className="txt-s font-semibold">Trade-athon</span>  is our biggest and best
               game yet, with prizes to be won twice a week, EVERY week!
