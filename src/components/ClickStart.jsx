54c28bd6b986ffe42737d94c248cc143f6791d5c
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-19 18:13:09 +0530

    Update ClickStart.jsx

--- src/components/ClickStart.jsx
+++ src/components/ClickStart.jsx
@@ -5,7 +5,7 @@ function ClickStart() {
     <section className=" relative -mt-[5%] sm:-mt-[1%]">
         <div className=" mx-auto  justify-center mt-4 mb-4 hidden sm:flex">
             <a
-              href="/"
+              href="https://test.kix.digital/?utm_source=landingpage&utm_medium=website&utm_campaign=tradeathon&utm_content=web3&utm_term=1millionkixtokens" target="_blank"
               className=" uppercase archo-black text-lg sm:text-xl txt-s py-3 px-12 rounded-lg  bg-[#11cac9]  text-[#edd400] mx-auto btn-hov "
             >
               {" "}
