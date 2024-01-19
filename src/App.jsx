1a700a9ba54d9db0e43a7bcbb43f9d96befd8e1a
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-01-19 17:58:06 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,37 @@ import About from "./components/About";
 import BuySell from "./components/BuySell";
 import ClickStart from "./components/ClickStart";
 import Hero from "./components/Hero";
+import { Bars } from 'react-loader-spinner'
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // 1000 milliseconds = 1 second
+
+    // Clear the timeout if the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <div className="main overflow-y-hidden">
+  {loading && (
+        <div className="loader flex justify-center items-center bg-[#6dc5f8] main h-[100vh]">
+          <Bars
+            height="100"
+            width="100"
+            visible={true}
+            color="#33eceb"
+            ariaLabel="falling-circles-loading"
+            wrapperStyle
+            wrapperClass
+          />
+        </div>
+      )}
+      <div className={`main overflow-y-hidden ${loading ? 'hidden' : ''}`}>
         <div className=" py-3">
           <Hero />
         </div>
@@ -16,7 +42,6 @@ function App() {
         <div className=" py-1">
           <ClickStart />
         </div>
-       
         <div className=" py-2">
           <About />
         </div>
